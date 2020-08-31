9ad20256239b26ab8277794a22d0a42391cf254f
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-08-31 16:57:09 +0200

    first commit

---
+++ WebApi3/WebApi3/Controller/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi3.Models;
+
+namespace WebApi3.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+                Id=1006368,
+                Name="Austin and barbeque AABQ wifi Food thermometer",
+                Description="Thermometer wifi for optimal innerTemperature",
+                Price=399
+            },
+            new Product
+            {
+                Id=1009334,
+                Name="Andesson electric tandare ECL 1.1",
+                Description="Electric stormsaker tandare  helt utan gas and bransle",
+                Price=89
+            },
+            new Product
+            {
+                Id=1002266,
+                Name="Weber nostick spray",
+                Description="BBQ som motverkar att ravaror fastnar pa gallret",
+                Price=99
+            }
+
+
+        };
+
+        [HttpGet]//Get all products
+
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)//get specific product
+        {
+            var product = products.Find(product => product.Id == id);
+            return product;
+        }
+
+        //Add new product
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+            products.Add(product);
+        }
+
+        //Delete product
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var product = products.Where(p => p.Id == id);
+            products = products.Except(product).ToList();
+        }
+
+        //Update product
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody] Product product)
+        {
+            var existingProduct = products.Where(p => p.Id == id);
+            products = products.Except(existingProduct).ToList();
+
+            products.Add(product);
+        }
+    }
+}
---
+++ webApi2/webApi2/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApi2.Models;
+
+namespace webApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+                Id=1006368,
+                Name="Austin and barbeque AABQ wifi Food thermometer",
+                Description="Thermometer wifi for optimal innerTemperature",
+                Price=399  
+            },
+            new Product
+            {
+                Id=1009334,
+                Name="Andesson electric tandare ECL 1.1",
+                Description="Electric stormsaker tandare  helt utan gas and bransle",
+                Price=89
+            },
+            new Product
+            {
+                Id=1002266,
+                Name="Weber nostick spray",
+                Description="BBQ som motverkar att ravaror fastnar pa gallret",
+                Price=99
+            }
+
+
+        };
+
+        [HttpGet]//Get all products
+       
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)//get specific product
+        {
+            var product = products.Find(product => product.Id == id);
+            return product;
+        }
+
+
+    }
+}
